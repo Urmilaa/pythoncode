e7344b02f87ce34e8eb3d42ac9d1dfa4044343a9
Urmilaa <urmikher@gmail.com>
2026-07-20 21:48:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print("Jenkin to GitHub integration Demo !!!Session 3!!!continuous Build")
 
 print("Github webhook Integration with jenkins!!!Session 4!!!continuous Build")
 
+print("Github Poll SCM !continuous Build")
+
